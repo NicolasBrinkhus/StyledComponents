2bbda87a9df95b7477870bdd1dea3de2b0a795e8
NicolasBrinkhus <nicolasbrinkhus@gmail.com>
2024-03-30 14:01:27 +0100

    add rettier, update packages, sync new router, add web vitails

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
-import App from "./App";
-import FormPage from "./Forms/FormPage";
-import TablesPage from "./TablesPage";
-import NavPage from "./Navegation/NavPage";
-import NotFound from "./NotFound";
-import FrontPage1 from "./FrontPages/FrontPage1";
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import App from './App'
+import FormPage from './Forms/FormPage'
+import TablesPage from './TablesPage'
+import NavPage from './Navegation/NavPage'
+import NotFound from './NotFound'
+import FrontPage1 from './FrontPages/FrontPage1'
 
 const theme = {
-  red: "#FF0000",
-  green: "#55c57a",
-  greenlight: "#7ed56f",
-  greendark: " #28b485",
-  black: "#393939",
-  lightblack: "#777777",
-  coral: "#ff7f50",
-  grey: "#3A3A3A",
-  lightgrey: "#E1E1E1",
-  offWhite: "#EDEDED",
-  maxWidth: "1000px",
-  orangelight: "#ffb900",
-  orangedark: "#ff7730",
-  bluelight: "#2998ff",
-  bluedark: "#5643fa",
-  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)",
-};
+  red: '#FF0000',
+  green: '#55c57a',
+  greenlight: '#7ed56f',
+  greendark: ' #28b485',
+  black: '#393939',
+  lightblack: '#777777',
+  coral: '#ff7f50',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  orangelight: '#ffb900',
+  orangedark: '#ff7730',
+  bluelight: '#2998ff',
+  bluedark: '#5643fa',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -46,28 +46,24 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     color: ${theme.black};
   }
-`;
+`
 
-class Router extends React.Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={App} />
-              <Route exact path="/front1" component={FrontPage1} />
-              <Route exact path="/forms" component={FormPage} />
-              <Route exact path="/tables" component={TablesPage} />
-              <Route exact path="/navegation" component={NavPage} />
-              <Route component={NotFound} />
-            </Switch>
-          </BrowserRouter>
-        </>
-      </ThemeProvider>
-    );
-  }
+const RouterApp = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/front1" element={<FrontPage1 />} />
+          <Route path="/forms" element={<FormPage />} />
+          <Route path="/tables" element={<TablesPage />} />
+          <Route path="/navegation" element={<NavPage />} />
+          <Route element={NotFound} />
+        </Routes>
+      </>
+    </ThemeProvider>
+  )
 }
 
-export default Router;
+export default RouterApp
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Router from "./components/Router";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Router from './components/Router'
+import reportWebVitals from './reportWebVitals'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
+serviceWorker.unregister()
